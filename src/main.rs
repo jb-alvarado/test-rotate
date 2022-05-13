47138f4ba86fb27782625c2bbf6da8f5f3850b45
jb-alvarado <jb@pixelcrusher.de>
2022-05-13 10:57:52 +0200

    simulate older files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::{Duration, SystemTime};
+
 use file_rotate::{
     compression::Compression,
     suffix::{AppendTimestamp, DateFrom, FileLimit},
@@ -7,17 +9,30 @@ use simplelog::*;
 
 fn main() {
     let log_path = "logs/test.log".to_string();
-    std::fs::File::create("logs/test.log.2022-05-01").unwrap();
-    std::fs::File::create("logs/test.log.2022-05-02").unwrap();
-    std::fs::File::create("logs/test.log.2022-05-03").unwrap();
-    std::fs::File::create("logs/test.log.2022-05-04").unwrap();
+    std::fs::File::create(&log_path).unwrap();
+    std::fs::File::create("logs/test.log.2022-05-01_09-45").unwrap();
+    std::fs::File::create("logs/test.log.2022-05-02_09-45").unwrap();
+    std::fs::File::create("logs/test.log.2022-05-03_09-45").unwrap();
+    std::fs::File::create("logs/test.log.2022-05-04_09-45").unwrap();
+
+    let houre_before = SystemTime::now() - Duration::from_secs(3600);
+
+    filetime::set_file_mtime(
+        &log_path,
+        filetime::FileTime::from_system_time(houre_before),
+    )
+    .unwrap();
 
     let log_config = ConfigBuilder::new().build();
 
     let log_file = FileRotate::new(
         &log_path,
-        AppendTimestamp::with_format("%Y-%m-%d", FileLimit::MaxFiles(1), DateFrom::DateYesterday),
-        ContentLimit::Time(TimeFrequency::Daily),
+        AppendTimestamp::with_format(
+            "%Y-%m-%d_%H-%M",
+            FileLimit::MaxFiles(1),
+            DateFrom::DateHourAgo,
+        ),
+        ContentLimit::Time(TimeFrequency::Hourly),
         Compression::None,
     );
 
